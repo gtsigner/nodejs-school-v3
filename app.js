2893123fa84a303bec6efc930bbe1c2e0436f8ff
1716771371@qq.com <1716771371@qq.com>
2016-07-04 08:12:19 +0800

    version-3  pages

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+/*post数据获取模块*/
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,13 +19,13 @@ var app = express();
 
 // 模板引擎安装
 app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(__dirname, 'public/images/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
 app.use(cookieParser());
 
 /*静态服务*/
